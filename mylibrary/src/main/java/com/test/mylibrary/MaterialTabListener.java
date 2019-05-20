e32a86c6296755052453e9c14f6c22d7f05eb7c8
jianwei <jianwei.chen@microvirt.com>
2019-05-20 16:59:57 +0800

    Add tabhost custom view
    Signed-off-by: jianwei <jianwei.chen@microvirt.com>

---
+++ mylibrary/src/main/java/com/test/mylibrary/MaterialTabListener.java
@@ -0,0 +1,9 @@
+package com.test.mylibrary;
+
+public interface MaterialTabListener {
+	public void onTabSelected(MaterialTab tab);
+	
+	public void onTabReselected(MaterialTab tab);
+	
+	public void onTabUnselected(MaterialTab tab);
+}
