3fcce5dcbf629fde895a00eefba5ae522d8b6d4f
Jianwei Chen <jianwei.chen@microvirt.com>
2019-11-01 11:45:35 +0800

    Add inject
    Signed-off-by: Jianwei Chen <jianwei.chen@microvirt.com>

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -12,3 +12,26 @@ LOCAL_SRC_FILES += emulator_check.c     \
 LOCAL_SRC_FILES += utils.c
 
 include $(BUILD_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_LDLIBS    := -llog
+
+LOCAL_MODULE    := hello
+LOCAL_SRC_FILES := hello1.cpp
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_LDLIBS    := -llog
+
+LOCAL_CFLAGS += -pie -fPIE
+LOCAL_LDFLAGS += -pie -fPIE
+
+LOCAL_MODULE    := inject
+LOCAL_SRC_FILES := inject.c
+
+include $(BUILD_EXECUTABLE)
